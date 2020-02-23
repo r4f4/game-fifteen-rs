6edda33595cc496564f0e46b04ddcc015da1d776
Rafael Fonseca <r4f4rfs@gmail.com>
2020-02-23 11:29:39 +0100

    main: use if-else instead of match for boolean

--- src/main.rs
+++ src/main.rs
@@ -44,30 +44,29 @@ Example:
         )
         .get_matches();
 
-    let mut board = match matches.is_present("random") {
-        true => Board::new_random(),
-        false => {
-            let mut buffer = String::new();
-            match io::stdin().read_to_string(&mut buffer) {
-                Err(err) => panic!("IO error: {}", err),
-                Ok(_) => {
-                    let mut config: Vec<u8> = Vec::new();
-                    let lines: Vec<&str> = buffer.split('\n').collect();
-                    for line in lines {
-                        let mut v: Vec<&str> = line.split(' ').collect();
-                        v.retain(|&x| x != "");
-                        let mut l: Vec<u8> = v
-                            .iter()
-                            .map(|x| x.parse::<u8>().expect("failed to parse number"))
-                            .collect();
-                        config.append(&mut l);
-                    }
-                    match Board::new_from(config.as_slice()) {
-                        Ok(b) => b,
-                        Err(msg) => {
-                            eprintln!("Invalid board: {}", msg);
-                            process::exit(1)
-                        }
+    let mut board = if matches.is_present("random") {
+        Board::new_random()
+    } else {
+        let mut buffer = String::new();
+        match io::stdin().read_to_string(&mut buffer) {
+            Err(err) => panic!("IO error: {}", err),
+            Ok(_) => {
+                let mut config: Vec<u8> = Vec::new();
+                let lines: Vec<&str> = buffer.split('\n').collect();
+                for line in lines {
+                    let mut v: Vec<&str> = line.split(' ').collect();
+                    v.retain(|&x| x != "");
+                    let mut l: Vec<u8> = v
+                        .iter()
+                        .map(|x| x.parse::<u8>().expect("failed to parse number"))
+                        .collect();
+                    config.append(&mut l);
+                }
+                match Board::new_from(config.as_slice()) {
+                    Ok(b) => b,
+                    Err(msg) => {
+                        eprintln!("Invalid board: {}", msg);
+                        process::exit(1)
                     }
                 }
             }
@@ -81,9 +80,10 @@ Example:
     match Astar::run(&board) {
         Some(moves) => {
             println!("Number of moves needed: {}", moves.len());
-            match matches.is_present("replay") {
-                true => print_game_replay(&mut board, moves),
-                false => println!("{:?}", moves),
+            if matches.is_present("replay") {
+                print_game_replay(&mut board, moves);
+            } else {
+                println!("{:?}", moves);
             }
         }
         None => println!("Could not solve board"),
